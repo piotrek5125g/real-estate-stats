51effcb24b5f19cbb9b961dc5dd5944654495f2c
piotr.nicewicz <piotr.nicewicz@ableneo.com>
2024-02-12 19:41:56 +0100

    cleanup

--- src/main/java/pl/pni/realestatestats/repository/HouseRepository.java
+++ src/main/java/pl/pni/realestatestats/repository/HouseRepository.java
@@ -10,16 +10,11 @@ import pl.pni.realestatestats.model.Region;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface HouseRepository extends JpaRepository<House, Long>, JpaSpecificationExecutor<House> {
+public interface HouseRepository extends JpaRepository<House, String>, JpaSpecificationExecutor<House> {
 
     @Query("SELECT h.houseId FROM House h WHERE h.region= :regionId")
     List<String> findHouseIdsByRegion(Region regionId);
 
     House findByHouseId(String houseId);
 
-//    @Query("SELECT AVG(h.price) FROM House h WHERE h.size= :size and h.region= :regionId " +
-//            "and h.rooms= :rooms and h.type IN(:types) and h.addedDate BETWEEN :dateFrom AND :dateTo")
-//    Double findAvgPrice(@Param("regionId") Region regionId, @Param("size") String size,
-//                        @Param("rooms") int rooms, @Param("types") List<String> types,
-//                        @Param("dateFrom") LocalDateTime dateFrom, @Param("dateTo") LocalDateTime dateUntil);
 }
